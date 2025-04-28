2e6ae86256b919b7b60a4e5a7f5fc15f11176b2c
KIMHYEOK\borad <kimmang940715@naver.com>
2025-04-29 02:22:23 +0900

    데이터 그리드뷰 및 콤보박스 사용법

--- MES/Form1.Designer.cs
+++ MES/Form1.Designer.cs
@@ -32,6 +32,11 @@
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
@@ -69,17 +74,59 @@
             this.label2.TabIndex = 3;
             this.label2.Text = "label2";
             // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(539, 66);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(537, 154);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "label3";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(179, 211);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(481, 155);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(180, 385);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(239, 40);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -91,6 +138,10 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- MES/Form1.cs
+++ MES/Form1.cs
@@ -15,11 +15,47 @@ namespace MES
         public Form1()
         {
             InitializeComponent();
+
+            comboBox1.Items.Add("대한민국");
+            comboBox1.Items.Add("중국");
+            comboBox1.Items.Add("일본");
+            comboBox1.Items.Add("인도");
+            comboBox1.Items.Add("미국");
+            comboBox1.Items.Add("대만");
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             label2.Text = textBox1.Text;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(comboBox1.SelectedItem != null)
+            {
+                string selecter = comboBox1.SelectedItem.ToString();
+                label3.Text = selecter;
+            }
+            else
+            {
+                label3.Text = "나라를 선택해 주세요";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("생산지시번호");
+            dt.Columns.Add("품목명");
+            dt.Columns.Add("수량");
+
+            // 샘플 데이터
+            dt.Rows.Add("WD-001", "제품A", 100);
+            dt.Rows.Add("WD-002", "제품B", 200);
+            dt.Rows.Add("WD-003", "제품C", 300);
+
+            // DataGridView 할당
+            dataGridView1.DataSource = dt;
         }
     }
 }
